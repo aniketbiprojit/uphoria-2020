56d2a804b5c9fe0806c7303ee91e62591d28a720
Aaryan-kapur <44099242+Aaryan-kapur@users.noreply.github.com>
2019-11-13 01:42:51 +0530

    edit1

--- client/src/scripts/home/Upper.jsx
+++ client/src/scripts/home/Upper.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-import "../../styles/home/Upper.scss";
+import '../../styles/home/Upper.scss'
 // import Upperimg from '../../assets/1.jpg'
 class Upper extends Component {
 	state = {}
 	render() {
 		return (
 			<React.Fragment>
-                <div className="Upper">
+				<div className="Upper">
+					<div className="navUpper">
+						<div className="right">
+							<div className="row">
+								<div className="col-sm-4 col-md-4"><span className="nav">Sponsors</span></div>
+								<div className="col-sm-4 col-md-4"><span className="nav">Register</span></div>
+								<div className="col-sm-4 col-md-4"><span className="nav">Artists</span></div>
 
-			
-                </div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</React.Fragment>
 		)
 	}
